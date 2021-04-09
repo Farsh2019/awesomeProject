8a5d67ae2f596724253daebaf9f52a3cf6ccf51a
gr603_issal <sergej_serega_isaev@mail.ru>
2021-04-09 17:04:23 +0700

    qq1

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Hello, world!")
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(""+port, nil)
 }
